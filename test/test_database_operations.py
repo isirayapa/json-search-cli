a3874113c6305d946644d009a783ee514576d0aa
isirayapa <isirayapa@gmail.com>
2019-10-11 21:49:43 +0530

    unit testing for db operations

--- test/test_database_operations.py
+++ test/test_database_operations.py
@@ -1,3 +1,36 @@
-def test_add():
-    assert True
-    assert False
+from database_operations import json_to_database, clear_database
+import pytest
+from database_operations import get_data
+
+
+def test_main():
+    json_to_database()
+    output = get_data(1, '_id', 71)
+    assert output is not None
+
+
+def test_invalid_class_name():
+    with pytest.raises(Exception):
+        assert get_data(5, '_id', 71)
+
+
+def test_invalid_term():
+    with pytest.raises(Exception):
+        assert get_data(1, 'INVALID_NAME', 71)
+
+
+def test_invalid_value():
+    with pytest.raises(Exception):
+        assert get_data(1, '_id', 9999999)
+    assert get_data(1, '_id', 9999999) == []
+
+
+def test_different_value_formats():
+    assert get_data(1, '_id', 71)
+    assert get_data(1, '_id', '71')
+
+
+def test_end():
+    clear_database()
+    output = get_data(1, '_id', 71)
+    assert output == []
